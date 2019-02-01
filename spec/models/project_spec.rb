ba06fea999a70d4eedf4dcab68c39820535c7830
tongari <info@tongari-as.com>
2019-02-01 10:09:26 +0900

    4:extends & trait

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -49,4 +49,29 @@ RSpec.describe Project, type: :model do
     # project.valid?
     # expect(project.errors[:name]).to include "can't be blank"
   end
+
+
+  # 遅延ステータス
+  describe 'late status' do
+    # 締切日がすぎていれば遅延していること
+    it 'is late when the due date is past today' do
+      # project = FactoryBot.create(:project_due_yesterday)
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
+
+    # 締切日が今日ならスケジュールどおりであること
+    it 'is on time when the due date is today' do
+      # project = FactoryBot.create(:project_due_today)
+      project = FactoryBot.create(:project,:due_today)
+      expect(project).to_not be_late
+    end
+
+    # 締切日が未来ならスケジュールどおりであること
+    it 'is on time when the due date is in the future' do
+      # project = FactoryBot.create(:project_due_tomorrow)
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
+  end
 end
